effe120b4f8a81b6e77b67d2bee7d7ba05cb9153
dennuguyen <dennuguyen@gmail.com>
2022-08-21 16:49:38 +1000

    Solve regular state to regular state for 3 pegs.

--- hanoi_solver.hpp
+++ hanoi_solver.hpp
@@ -7,24 +7,55 @@
 #include <utility>
 #include <vector>
 
+template <typename T>
+auto operator<<(std::ostream& os, std::vector<T> const& v) -> std::ostream& {
+    os << "[";
+    std::copy(v.begin(), v.end(), std::experimental::make_ostream_joiner(os, ", "));
+    os << "]";
+    return os;
+}
+
 std::vector<std::pair<int, int>> moves;
 
-auto hanoi(int num_discs, int disc, int from, int to, int free) {
-    if (num_discs == 0) {
+auto hanoi(std::vector<int> from, std::vector<int> to) {
+    if (from.empty()) {
+        // Base case after placing all discs in their rightful positions.
         return;
+    } else if (from.at(0) == to.at(0)) {
+        // Base case for placing the largest disc at its rightful position.
+        hanoi({from.begin() + 1, from.end()}, {to.begin() + 1, to.end()});
+    } else {
+        // Determine which pegs are free i.e. not from and to pegs.
+        auto pegs = std::set<int>({1, 2, 3});
+        pegs.erase(from.at(0));
+        pegs.erase(to.at(0));
+        auto free = std::vector<int>(from.size() - 1, *pegs.begin());
+
+        // Keep exploring subsolutions.
+        hanoi({from.begin() + 1, from.end()}, free);
+        moves.emplace_back(std::make_pair(from.at(0), to.at(0)));
+        hanoi(free, {to.begin() + 1, to.end()});
     }
-    hanoi(num_discs - 1, disc, from, free, to);
-    moves.emplace_back(std::make_pair(from, to));
-    hanoi(num_discs - 1, disc, free, to, from);
 }
 
-auto reves(int num_discs, int from, int to, int free1, int free2) {
-    int partition_disc = num_discs + 1 - std::round(std::sqrt(2 * num_discs + 1));
-    if (partition_disc == 0) {
-        moves.emplace_back(std::make_pair(from, to));
-        return;
-    }
-    reves(partition_disc, from, free1, to, free2);
-    hanoi(num_discs - partition_disc, partition_disc, from, to, free2);
-    reves(partition_disc, free1, to, from, free2);
-}
+/////////////// EVERYTHING BELOW WORKS FOR REVE'S with 4 PEGS AND N DISCS.
+
+// auto hanoi(int num_discs, int disc, int from, int to, int free) {
+//     if (num_discs == 0) {
+//         return;
+//     }
+//     hanoi(num_discs - 1, disc, from, free, to);
+//     moves.emplace_back(std::make_pair(from, to));
+//     hanoi(num_discs - 1, disc, free, to, from);
+// }
+
+// auto reves(int num_discs, int from, int to, int free1, int free2) {
+//     int partition_disc = num_discs + 1 - std::round(std::sqrt(2 * num_discs + 1));
+//     if (partition_disc == 0) {
+//         moves.emplace_back(std::make_pair(from, to));
+//         return;
+//     }
+//     reves(partition_disc, from, free1, to, free2);
+//     hanoi(num_discs - partition_disc, partition_disc, from, to, free2);
+//     reves(partition_disc, free1, to, from, free2);
+// }
--- test_hanoi_solver.cpp
+++ test_hanoi_solver.cpp
@@ -7,97 +7,108 @@
 #include "hanoi_simulator.hpp"
 #include "hanoi_solver.hpp"
 
-template <typename T>
-auto operator<<(std::ostream& os, std::vector<T> const& v) -> std::ostream& {
-    os << "[";
-    std::copy(v.begin(), v.end(), std::experimental::make_ostream_joiner(os, ", "));
-    os << "]";
-    return os;
+TEST_CASE("3 Pegs.") {
+    auto num_pegs = 3;
+    auto initial = std::vector<int>();
+    auto final = std::vector<int>();
+
+    SUBCASE("[1, 1, 1] -> [1, 1, 1]") {
+        initial = std::vector<int>({1, 1, 1});
+        final = std::vector<int>({1, 1, 1});
+    }
+
+    SUBCASE("[1, 1, 1] -> [2, 2, 2]") {
+        initial = std::vector<int>({1, 1, 1});
+        final = std::vector<int>({2, 2, 2});
+    }
+
+    SUBCASE("[1, 1, 1] -> [3, 3, 3]") {
+        initial = std::vector<int>({1, 1, 1});
+        final = std::vector<int>({3, 3, 3});
+    }
+
+    SUBCASE("[1, 1, 1] -> [1, 2, 3]") {
+        initial = std::vector<int>({1, 1, 1});
+        final = std::vector<int>({1, 2, 3});
+    }
+
+    SUBCASE("[1, 1, 1, 1] -> [2, 1, 2, 3]") {
+        initial = std::vector<int>({1, 1, 1, 1});
+        final = std::vector<int>({2, 1, 2, 3});
+    }
+
+    SUBCASE("[1, 1, 1, 1] -> [3, 3, 3, 3]") {
+        initial = std::vector<int>({1, 1, 1, 1});
+        final = std::vector<int>({3, 3, 3, 3});
+    }
+
+    SUBCASE("[1, 1, 1, 1] -> [3, 3, 3, 2]") {
+        initial = std::vector<int>({1, 1, 1, 1});
+        final = std::vector<int>({3, 3, 3, 2});
+    }
+
+    SUBCASE("[1, 1, 1, 1, 1] -> [3, 3, 1, 2, 3]") {
+        initial = std::vector<int>({1, 1, 1, 1, 1});
+        final = std::vector<int>({3, 3, 1, 2, 3});
+    }
+
+    SUBCASE("[2, 1, 3, 2, 1] -> [1, 3, 1, 2, 1]") {
+        initial = std::vector<int>({2, 1, 3, 2, 1});
+        final = std::vector<int>({1, 3, 1, 2, 1});
+    }
+
+    SUBCASE("[3, 1, 3, 1, 1, 3, 2] -> [2, 2, 2, 2, 2, 2, 2]") {
+        initial = std::vector<int>({3, 1, 3, 1, 1, 3, 2});
+        final = std::vector<int>({2, 2, 2, 2, 2, 2, 2});
+    }
+
+    hanoi(initial, final);
+    auto hs = hanoi_simulator(initial, num_pegs);
+    for (auto const& i : moves) {
+        hs.move(i.first, i.second);
+    }
+
+    CHECK(std::vector<int>(hs) == final);
+
+    moves.clear();
 }
 
-// TEST_SUITE("Hanoi with 3 pegs and 4 discs.") {
-// TEST_CASE("[1, 1, 1, 1] -> [3, 3, 3, 3]") {
-//     auto num_pegs = 3;
-//     auto initial = std::vector<int>({1, 1, 1, 1});
-//     auto final = std::vector<int>({3, 3, 3, 3});
-
-//     // hanoi(initial.size(), initial, final, num_pegs);
-//     reves(4, 1, 2, 3, 4);
-//     auto hs = hanoi_simulator(initial, num_pegs);
-//     for (auto const& i : moves) {
-//         hs.move(i.first, i.second);
-//     }
-
-//     CHECK(std::vector<int>(hs) == final);
-// }
-
-// TEST_CASE("[1, 1, 1, 1] -> [3, 3, 3, 2]") {
-//     auto num_pegs = 3;
-//     auto initial = std::vector<int>({1, 1, 1, 1});
-//     auto final = std::vector<int>({3, 3, 3, 2});
-
-//     hanoi(initial.size(), initial, final, num_pegs);
-//     auto hs = hanoi_simulator(initial, num_pegs);
-//     for (auto const& i : moves) {
-//         hs.move(i.first, i.second);
-//     }
-
-//     CHECK(std::vector<int>(hs) == final);
-// }
-// }
-
-// TEST_SUITE("Hanoi with 3 pegs and 5 discs.") {
-//     TEST_CASE("[1, 1, 1, 1, 1] -> [3, 3, 1, 2, 3]") {
-//         auto num_pegs = 3;
+// TEST_SUITE("4 Pegs.") {
+//     TEST_CASE("[1, 1, 1, 1, 1] -> [3, 3, 3, 3, 3]") {
+//         auto num_pegs = 4;
 //         auto initial = std::vector<int>({1, 1, 1, 1, 1});
-//         auto final = std::vector<int>({3, 3, 1, 2, 3});
+//         auto final = std::vector<int>({3, 3, 3, 3, 3});
 
-//         hanoi(initial.size(), initial, final, num_pegs);
+//         reves(initial.size(), 1, 3, 2, 4);
 //         auto hs = hanoi_simulator(initial, num_pegs);
 //         for (auto const& i : moves) {
 //             hs.move(i.first, i.second);
 //         }
 
+//         std::cout << hs << std::endl;
+//         std::cout << final << std::endl;
+
 //         CHECK(std::vector<int>(hs) == final);
+
+//         moves.clear();
+//     }
+
+//     TEST_CASE("[1, 1, 1, 1, 1, 1, 1] -> [2, 2, 2, 2, 2, 2, 2]") {
+//         auto num_pegs = 4;
+//         auto initial = std::vector<int>({1, 1, 1, 1, 1, 1, 1});
+//         auto final = std::vector<int>({2, 2, 2, 2, 2, 2, 2});
+
+//         reves(initial.size(), 1, 2, 3, 4);
+//         auto hs = hanoi_simulator(initial, num_pegs);
+//         for (auto const& i : moves) {
+//             hs.move(i.first, i.second);
+//         }
+
+//         std::cout << hs << std::endl;
+//         std::cout << final << std::endl;
+
+//         CHECK(std::vector<int>(hs) == final);
+
+//         moves.clear();
 //     }
 // }
-
-TEST_SUITE("4 Pegs.") {
-    TEST_CASE("[1, 1, 1, 1, 1] -> [3, 3, 3, 3, 3]") {
-        auto num_pegs = 4;
-        auto initial = std::vector<int>({1, 1, 1, 1, 1});
-        auto final = std::vector<int>({3, 3, 3, 3, 3});
-
-        reves(initial.size(), 1, 3, 2, 4);
-        auto hs = hanoi_simulator(initial, num_pegs);
-        for (auto const& i : moves) {
-            hs.move(i.first, i.second);
-        }
-
-        std::cout << hs << std::endl;
-        std::cout << final << std::endl;
-
-        CHECK(std::vector<int>(hs) == final);
-
-        moves.clear();
-    }
-
-    TEST_CASE("[1, 1, 1, 1, 1, 1, 1] -> [2, 2, 2, 2, 2, 2, 2]") {
-        auto num_pegs = 4;
-        auto initial = std::vector<int>({1, 1, 1, 1, 1, 1, 1});
-        auto final = std::vector<int>({2, 2, 2, 2, 2, 2, 2});
-
-        reves(initial.size(), 1, 2, 3, 4);
-        auto hs = hanoi_simulator(initial, num_pegs);
-        for (auto const& i : moves) {
-            hs.move(i.first, i.second);
-        }
-
-        std::cout << hs << std::endl;
-        std::cout << final << std::endl;
-
-        CHECK(std::vector<int>(hs) == final);
-
-        moves.clear();
-    }
-}
